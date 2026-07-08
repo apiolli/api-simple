60045a3a76bc5770258a114366de20f6021233d4
apiolli <cesardiaz.miduho@gmail.com>
2026-07-08 00:14:12 -0400

    api structure added

---
+++ backend/Controllers/MascotasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_simple.Controllers
+{
+    [Route("api/mascotas")]
+    [ApiController]
+    public class MascotasController : ControllerBase
+    {
+    }
+}
---
+++ backend/Controllers/PersonasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_simple.Controllers
+{
+    [Route("api/personas")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+    }
+}
